b3e1bb15758170d794aaede28c6ba3648fa0493b
unknown <zz240614@gmail.com>
2025-01-13 14:20:00 +0100

    Creat Class Etudiant extend class user Whit methods

---
+++ app/controller/Etudiant.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once('./app/controller/Utilisateur.php');
+
+class Etudiant extends Utilisateur {
+
+
+
+    public function inscriptionCours(){
+
+    }
+
+    public function accesMesCoursSection(){
+
+    }
+}
+
