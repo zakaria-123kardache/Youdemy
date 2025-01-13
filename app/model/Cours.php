180e2da05e36842c6e574481325c34589b85602c
unknown <zz240614@gmail.com>
2025-01-13 17:00:03 +0100

    Creat Cours Class with geters and seters

---
+++ app/model/Cours.php
@@ -0,0 +1,93 @@
+<?php
+
+
+
+class Cours {
+    private int $id ; 
+    private string $name ; 
+    private string $description ;
+    private string $photo ; 
+    private string $contenu ; 
+    private array $tags = [] ; 
+    private Categorie $categorie ; 
+    private Enseignant $enseignant ; 
+
+    public function __construct(){}
+
+
+    public function getId(int $id) : void
+    {
+        $this->id = $id;
+    }
+    public function getName(string $name) : void
+    {
+        $this->name = $name;
+    }
+    public function getDescription(string $description) : void
+    {
+        $this->description = $description;
+    }
+
+    public function getPhoto(string $photo) : void
+    {
+        $this->photo = $photo;
+    }
+
+    public function getContenu(string $contenu) : void
+    {
+        $this->contenu = $contenu;
+    }
+
+    public function getTags(array $tags) : void
+    {
+        $this->tags = $tags;
+    }
+
+    public function getCategorie(Categorie $categorie) : void
+    {
+        $this->categorie = $categorie;
+    }
+
+
+    public function getEnseignant(Enseignant $enseignant) : void
+    {
+        $this->enseignant = $enseignant;
+    }
+
+
+    public function setId() :int {
+        return $this->id;
+    }
+
+    public function setName() :string {
+        return $this->name;
+    }
+
+    public function setDescription() :string {
+        return $this->description;
+    }
+
+    public function setPhoto() :string {
+        return $this->photo;
+    }
+
+    public function setContenu() :string {
+        return $this->contenu;
+    }
+
+    public function setTags() : array
+    {
+       return $this->tags;
+    }
+
+    public function setCategorie() : Categorie
+    {
+       return $this->categorie ;
+    }
+
+    public function setEnseignant() : Enseignant {
+        return $this->enseignant;
+    }
+    
+}
+
