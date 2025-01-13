d0940cc0d0c1e469b581866d18d0f91f9687aa18
unknown <zz240614@gmail.com>
2025-01-13 14:32:58 +0100

    Creat class Enseignant extend Class user whit methods

---
+++ app/controller/Enseignant.php
@@ -0,0 +1,25 @@
+
+
+<?php
+
+require_once('./app/controller/Utilisateur.php');
+
+class Enseignant extends Utilisateur {
+
+
+    public function ajoutCour(){
+
+    }
+
+    public function suppresiionCour(){
+
+    }
+
+    public function modificationCour(){
+
+    }
+
+    public function accesStatiquefunction (){
+
+    }
+}
