cd52c3f08efeb35e3d5cc394923089738b8735f2
unknown <zz240614@gmail.com>
2025-01-13 14:51:31 +0100

    Creat Ecatag Class with proprities and geters and seters

---
+++ app/controller/Ecatag.php
@@ -0,0 +1,31 @@
+<?php
+
+class Ecatag {
+
+    private int $id ; 
+    private string $name ;
+    private string $description ; 
+    
+    public function __construct(){}
+
+    public function getId (int $îd):void{
+        $this->id = $îd ; 
+    }
+    public function getName (string $name):void{
+        $this->name = $name ; 
+    }
+    public function getDescription (string $description):void{
+        $this->description = $description ; 
+    }
+
+    public function setId() : int{
+        return $this->id ;  
+    }
+    public function setName() : string{
+        return $this->name ;  
+    }
+    public function setDescription() : string{
+        return $this->description ;  
+    }
+
+}
