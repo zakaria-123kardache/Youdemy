8224ed08a6edfc18dbbbe9d57a5756ea3fa276a1
unknown <zz240614@gmail.com>
2025-01-16 00:00:18 +0100

    creat Class admin whit methods

---
+++ app/model/Administrateur.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Model\Utilisateur;
+
+class Administrateur extends Utilisateur
+{
+
+    public function validationEnseignantAccount(){
+        
+    }
+    public function GestionUtilisateur(){
+
+    }
+    public function GestionContent(){
+
+    }
+    public function AccesStatistiqueGlobal(){
+
+    }
+}
---
+++ app/model/Administration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Model;
+
+class Administration extends Utilisateur {
+
+
+
+    public function validationEnseignantAccount(){
+
+    }
+
+    public function gestionUtilisateur(){
+
+    }
+    public function gestionContent(){
+
+    }
+    public function accesStatistiQueGlobal(){
+
+    }
+
+    public function __toString()
+    {
+        return parent:: __toString();
+    }
+}
+
