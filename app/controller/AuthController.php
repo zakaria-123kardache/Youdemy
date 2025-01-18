074dd1ead7e6c3419d0da7541d4179ede1311d11
unknown <zz240614@gmail.com>
2025-01-18 01:32:24 +0100

    creat class sign up in auth controller to use created method of user and handle user signUp

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -1,6 +1,50 @@
 <?php
 
+use App\Model\Role;
+use App\Model\Utilisateur;
+
 class AuthController {
 
-    
+    public function SignUp (){
+
+        $signupform = SignUpForm::instanceWithAllArgs(
+            $_POST['firstname'],
+            $_POST['lastname'],
+            $_POST['email'],
+            $_POST['password'],
+            $_POST['passwordConfirmation'],
+            $_POST['photo'],
+            $_POST['rolename']
+        );
+
+        if($signupform->password !== $signupform->passwordConfirmation){
+            echo " password nt corrected";
+            return;
+        }
+
+        $role = Role::instanceWithNameAndDescriptionAndLogo(
+            $signupform->rolename,
+            "admmiin",  
+            "path/to/logo.png"      
+        );
+
+        $utilisateur = Utilisateur::instance(
+            $signupform->firstname,
+            $signupform->lastname,
+            $signupform->email,
+            $signupform->password,
+            $signupform->passwordConfirmation,
+            $signupform->photo,
+            $role
+        );
+
+        $utilisateur = $utilisateur->create($utilisateur);
+
+        $_SESSION['user'] = $utilisateur;
+        exit();
+
+
+
+
+    }
 }
